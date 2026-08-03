d96c5f8d2a7cf89400c918b7525973dd9962785b
Daryl <donamedenou@gmail.com>
2026-08-03 17:20:29 +0200

    test(backend): add tests for application module structure * Implement tests to verify modular structure of the application * Print application modules for debugging purposes

--- backend/src/test/java/com/peithyra/api/BackendApplicationTests.java
+++ backend/src/test/java/com/peithyra/api/BackendApplicationTests.java
@@ -3,6 +3,7 @@ package com.peithyra.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.modulith.core.ApplicationModules;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -12,4 +13,15 @@ class BackendApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void createApplicationModuleModel() {
+        ApplicationModules modules = ApplicationModules.of(BackendApplication.class);
+        modules.forEach(System.out::println);
+    }
+
+    @Test
+    void verifiesModularStructure() {
+        ApplicationModules modules = ApplicationModules.of(BackendApplication.class);
+        modules.verify();
+    }
 }
