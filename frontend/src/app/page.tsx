2030cdc7b81a41f918ac3d676d5d3bdeb877f22c
Daryl M <donamedenou@gmail.com>
2026-09-13 12:31:14 +0200

    feat: update global styles and components for new design system
    - Refactored global CSS to use oklch color space for improved color management.
    - Introduced new UI components including Button, Badge, and Tabs with variants.
    - Enhanced layout structure in the main page for better responsiveness and user experience.
    - Added utility functions for class name management.
    - Integrated new fonts and updated metadata for the application.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,144 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Dot, MoveRight } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex flex-col flex-1 items-center max-w-6xl xl:max-w-7xl mx-auto w-full justify-center">
+			<header className="w-full">
+				<nav className="flex items-center justify-between p-8 border-b border-b-primary/10 py-4">
+					<h1 className="text-2xl font-bold">Peithyra</h1>
+					<div className="flex gap-4">
+						<Button variant="ghost" size="lg">
+							Sign In
+						</Button>
+						<Button variant="default" size="lg">
+							Create Account
+						</Button>
+					</div>
+				</nav>
+			</header>
+			<main className="flex-1 py-10 px-8 w-full flex flex-col gap-16">
+				<section className="flex flex-col gap-2 w-1/2 items-start">
+					<p className="uppercase text-muted-foreground  ">
+						Une proposition. Deux camps.
+					</p>
+					<h1 className="text-5xl font-medium leading-tight mt-2 mb-4">
+						Vos convictions méritent une vraie confrontation.
+					</h1>
+					<p className="text-muted-foreground  ">
+						Défendez ou contestez une proposition, à deux et à tour
+						de rôle. Ou lisez les échanges pour vous faire votre
+						propre opinion.
+					</p>
+
+					<Button variant="default" size="lg" className="mt-4">
+						Lancer une proposition
+						<MoveRight />
+					</Button>
+				</section>
+
+				<section  >
+					<h2 className="text-3xl font-medium leading-tight mt-2 mb-4">
+						Les propositions du moment
+					</h2>
+
+					<Tabs defaultValue="tous" >
+						<div className="border-b w-full border-b-primary/10">
+							<TabsList variant="line" className={"border-b lg:gap-10 border-b-primary/10 "} >
+								<TabsTrigger value="tous">Tous</TabsTrigger>
+								<TabsTrigger value="analytics">
+									A rejoindre
+								</TabsTrigger>
+								<TabsTrigger value="en_cours">En cours</TabsTrigger>
+								<TabsTrigger value="reports">Terminés</TabsTrigger>
+							</TabsList>
+						</div>
+
+						<TabsContent value="tous">
+							<div className="flex flex-col gap-4">
+								{ [...Array(2)].map((_, index) => (
+									<article key={index} className="flex flex-col gap-2 py-4 border-b border-b-primary/10">
+									<p className="text-muted-foreground uppercase">
+										Cherche une opposition
+									</p>
+									<h3 className="text-2xl font-medium leading-tight mt-2 mb-4">
+										Le réchauffement climatique est une menace pour l'humanité
+									</h3>
+
+									<div className="flex justify-between flex-col sm:flex-row sm:items-center gap-4">
+										<div className="flex gap-4 items-center">
+											<div className="flex gap-2">
+												<Badge variant="for">Pour</Badge>
+												<span>Sarah</span>
+											</div>
+											<MoveRight />
+											<div className="flex gap-2">
+												<Badge variant="against">Contre</Badge>
+												<span>???</span>
+											</div>
+										</div>
+										<Button variant="secondary" size="lg">
+											Voir la proposition
+											<MoveRight />
+										</Button>
+									</div>
+								</article>))
+								}
+								<article className="flex flex-col gap-2 py-4 border-b border-b-primary/10">
+									<p className="text-muted-foreground uppercase flex items-center">
+										<Dot color="green" />
+										En cours
+										-
+										Tour 2 / 6
+									</p>
+									<h3 className="text-2xl font-medium leading-tight mt-2 mb-4">
+										Le réchauffement climatique est une menace pour l'humanité
+									</h3>
+
+									<div className="flex justify-between flex-col sm:flex-row sm:items-center gap-4">
+										<div className="flex gap-4 items-center">
+											<div className="flex gap-2">
+												<Badge variant="for">Pour</Badge>
+												<span>Sarah</span>
+											</div>
+											<MoveRight />
+											<div className="flex gap-2">
+												<Badge variant="against">Contre</Badge>
+												<span>???</span>
+											</div>
+										</div>
+										<Button variant="secondary" size="lg">
+											Voir la proposition
+											<MoveRight />
+										</Button>
+									</div>
+								</article>
+								
+							</div>
+						</TabsContent>
+						<TabsContent value="analytics">
+							A rejoindre
+						</TabsContent>
+						<TabsContent value="en_cours">
+							En cours
+						</TabsContent>
+						<TabsContent value="reports">
+							Terminés
+						</TabsContent>
+					</Tabs>
+				</section>
+			</main>
+
+			<footer className="w-full">
+				<div className="flex items-center justify-between p-8 border-t border-t-primary/10 py-4">
+					<p className="text-muted-foreground text-sm">
+						Peithyra &copy; {new Date().getFullYear()} - Tous droits réservés
+					</p>
+				</div>
+			</footer>
+		</div>
+	);
 }
